2fe59cfc571e5fe80e9bc5ecd13a86a08ff02aef
Clair Rogers <clair-rogers@outlook.com>
2019-04-29 10:40:28 -0600

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntriesComponent } from './entries/entries.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EntriesComponent },
+  { path: 'entries', component: EntriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+//services
+import { EntryService } from './entry.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  //add service in providers
+  providers: [EntryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
